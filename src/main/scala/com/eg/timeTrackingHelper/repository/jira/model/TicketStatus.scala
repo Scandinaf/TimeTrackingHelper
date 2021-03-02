6ec351f3dc962f67b2c01edf49e2debd8935dbc9
Siarhei Barouski <sbarouski@evolutiongaming.com>
2021-03-02 22:10:49 +0300

    make the TicketStatus entity case insensitive

--- src/main/scala/com/eg/timeTrackingHelper/repository/jira/model/TicketStatus.scala
+++ src/main/scala/com/eg/timeTrackingHelper/repository/jira/model/TicketStatus.scala
@@ -1,3 +1,10 @@
 package com.eg.timeTrackingHelper.repository.jira.model
 
 case class TicketStatus(value: String)
+
+object TicketStatus {
+
+  def apply(value: String): TicketStatus =
+    new TicketStatus(value.toLowerCase())
+
+}
